42d68226e4365b99b4dca7a7866fed2d57b2e8f7
Wei Lin <373988402@qq.com>
2021-07-08 15:57:53 +0800

    fix bug

--- lib/utils/getScriptSrc.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getScriptSrc(config: MapConfig): string;
--- packages/services/lazy-map-loader.ts
+++ packages/services/lazy-map-loader.ts
@@ -1,4 +1,26 @@
-import { getScriptSrc } from '@vue3-amap/utils/getScriptSrc'
+function getScriptSrc(config: MapConfig) {
+  const paramKeys = ['v', 'key', 'plugin', 'callback']
+
+  const params = Object.keys(config)
+    .filter(k => ~paramKeys.indexOf(k))
+    .filter(k => config[k as keyof MapConfig] != null)
+    .filter(k => {
+      return (
+        !Array.isArray(config[k as keyof MapConfig]) ||
+        (Array.isArray(config[k as keyof MapConfig]) &&
+          (config[k as keyof MapConfig] as []).length > 0)
+      )
+    })
+    .map(k => {
+      const v = config[k as keyof MapConfig]
+      if (Array.isArray(v)) return { key: k, value: v.join(',') }
+      return { key: k, value: v }
+    })
+    .map(entry => `${entry.key}=${entry.value}`)
+    .join('&')
+
+  return `${config.protocol}://${config.hostAndPath}?${params}`
+}
 
 const MAP_SCRIPT_INIT_CALLBACK = 'mapScriptInitCallback'
 
