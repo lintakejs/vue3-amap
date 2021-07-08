98fcb92dbdf901ec561a7ec92ec6d762ae0cbfa4
Wei Lin <373988402@qq.com>
2021-07-08 14:25:56 +0800

    fix babel plugin component

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,10 @@
 import type { App } from 'vue';
-import VMap from './vmap';
-import VMapCirCle from './vmap-circle';
-import VMapMarker from './vmap-marker';
-import VMapPolygon from './vmap-polygon';
-import VMapPolyline from './vmap-polyline';
-import VMapText from './vmap-text';
+import VMap from './v-map';
+import VMapCirCle from './v-map-circle';
+import VMapMarker from './v-map-marker';
+import VMapPolygon from './v-map-polygon';
+import VMapPolyline from './v-map-polyline';
+import VMapText from './v-map-text';
 import { initMapApiLoader } from './services/injected-map-api';
 declare const install: (app: App) => void;
 export { VMap, VMapCirCle, VMapMarker, VMapPolygon, VMapPolyline, VMapText, install, initMapApiLoader, };
--- lib/vue3-amap/index.d.ts
+++ lib/vue3-amap/index.d.ts
@@ -1,10 +1,10 @@
 import type { App } from 'vue';
-import VMap from '@vue3-amap/vmap';
-import VMapCirCle from '@vue3-amap/vmap-circle';
-import VMapMarker from '@vue3-amap/vmap-marker';
-import VMapPolygon from '@vue3-amap/vmap-polygon';
-import VMapPolyline from '@vue3-amap/vmap-polyline';
-import VMapText from '@vue3-amap/vmap-text';
+import VMap from '@vue3-amap/v-map';
+import VMapCirCle from '@vue3-amap/v-map-circle';
+import VMapMarker from '@vue3-amap/v-map-marker';
+import VMapPolygon from '@vue3-amap/v-map-polygon';
+import VMapPolyline from '@vue3-amap/v-map-polyline';
+import VMapText from '@vue3-amap/v-map-text';
 import { initMapApiLoader } from '@vue3-amap/services/injected-map-api';
 declare const install: (app: App) => void;
 export { VMap, VMapCirCle, VMapMarker, VMapPolygon, VMapPolyline, VMapText, install, initMapApiLoader, };
