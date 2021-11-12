fc82b57bde01fdf0692bf98bce4531f8630fd61a
lin <linwei1@xiaohongshu.com>
2021-11-12 18:03:15 +0800

    fixed vitepress error bug

--- packages/services/injected-map-api.ts
+++ packages/services/injected-map-api.ts
@@ -4,7 +4,7 @@ let lazyMapApiLoaderInstance: MapApiLoader | null = null
 
 export const initMapApiLoader = function (config: MapConfig) {
   if (lazyMapApiLoaderInstance) {
-    throw new Error('You has already initial map instance, just import it!')
+    console.warn('You has already initial map instance, just import it!')
   }
   lazyMapApiLoaderInstance = new MapApiLoader(config)
 }
